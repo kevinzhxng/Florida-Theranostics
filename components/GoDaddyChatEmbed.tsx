fa1e27df86e9b8944a4dd831f2f37cc2b7ef0f12
Kevin Zhang <kzhang0826@gmail.com>
2026-02-16 18:30:00 -0500

    redeployment

--- components/GoDaddyChatEmbed.tsx
+++ components/GoDaddyChatEmbed.tsx
@@ -13,21 +13,20 @@ import {
 export default function GoDaddyChatEmbed() {
   useEffect(() => {
     // Run the inline config in global scope so Reamaze loader can read window._support
-    try {
-      const runInGlobal = (0, eval);
-      runInGlobal(GODADDY_CHAT_INLINE_SCRIPT);
-    } catch (e) {
-      console.warn("GoDaddy chat config error:", e);
-    }
+    const configScript = document.createElement("script");
+    configScript.type = "text/javascript";
+    configScript.textContent = GODADDY_CHAT_INLINE_SCRIPT;
+    document.head.appendChild(configScript);
 
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.async = true;
-    script.src = GODADDY_CHAT_LOADER_URL;
-    document.body.appendChild(script);
+    const loaderScript = document.createElement("script");
+    loaderScript.type = "text/javascript";
+    loaderScript.async = true;
+    loaderScript.src = GODADDY_CHAT_LOADER_URL;
+    document.body.appendChild(loaderScript);
 
     return () => {
-      if (script.parentNode) script.parentNode.removeChild(script);
+      configScript.remove();
+      loaderScript.remove();
     };
   }, []);
 
